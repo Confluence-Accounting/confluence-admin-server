b99c39471dd62d15427fb7de8da262245aa7f781
Michiel Valee <michiel@valee.be>
2018-05-01 21:51:18 +0200

    deployment fixes final

--- deploy.bat
+++ deploy.bat
@@ -1,7 +1,5 @@
 @ECHO OFF 
 call composer dump-autoload
-call php artisan clear-cache
-call php artisan clear-compiled
+call php artisan cache:clear
 call php artisan migrate:refresh --seed --force
 call php artisan route:cache
-call php artisan optimize --force
